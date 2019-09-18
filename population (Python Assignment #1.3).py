d3ed87ee176763a2267134db59d3f6bfcc18f78f
kfmarini <55286533+kfmarini@users.noreply.github.com>
2019-09-18 11:19:21 -0400

    Update population (Python Assignment #1.3).py
    User input for how many years have passed since the original year
    Calculate number of births by multiplying number of births per year and number of years
    Calculate number of deaths by multiplying number of deaths per year and number of years
    Calculate number of immigrants by multiplying number of immigrants per year and number of years
    Calculate new population by adding number of births and deaths, and subtracting the number of deaths, all from the original population
    Print the estimated population

--- population (Python Assignment #1.3).py
+++ population (Python Assignment #1.3).py
@@ -1,5 +1,6 @@
 #Kathryn Marini 9/17/19
 def population():
+    #This code calculates the estimated population with a user input for the year, using births, deaths, and immigrants summed up with the original population.
     years = float(input("How many years have passed?"))
     population = 307357870
     secondsToYears = 31536000
